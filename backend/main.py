319bee010bdfc0394a9436d348f826cf917ba793
holzit <holzerstefan@hotmail.com>
2021-10-19 19:30:41 +0200

    trying to evaluate some code wip

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,19 @@
 import uvicorn
+from pydantic import BaseModel
+from safeeval import SafeEval
 from fastapi import FastAPI
 
 app = FastAPI(root_path="/api")
+
+
+def solvepuzzle(code):
+    res = SafeEval.safeEval(code)
+    print(res)
+    return res
+
+
+class Code(BaseModel):
+    code: str
 
 
 @app.get("/")
@@ -17,6 +29,13 @@ async def api():
 @app.get("/test")
 async def test():
     return "testing!"
+
+
+@app.post("/submit")
+async def submit(code: Code):
+    print(code.code)
+    res = solvepuzzle(code.code)
+    return res
 
 
 if __name__ == "__main__":
