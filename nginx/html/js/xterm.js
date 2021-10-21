8867a79d2228b1f88e93f7290549d44143c31918
holzit <holzerstefan@hotmail.com>
2021-10-21 23:49:56 +0200

    xterm backspace and clear for adventure start

--- nginx/html/js/xterm.js
+++ nginx/html/js/xterm.js
@@ -6,19 +6,20 @@ let term = new Terminal({
 const terminal = document.getElementById('console');
 let myBuffer = [];
 let start = true;
+let del = false;
 
 term.open(terminal);
-term.prompt = () => {
-    term.write('\r\n$ ');
-};
 term.writeln('Welcome to \x1B[1;3;31mmudpy 1.0\x1B[0m')
 term.writeln('What would you like to do?')
-term.writeln('Press 1: Load from form previous session')
+term.writeln('Press 1: Load from previous session')
 term.writeln('Press 2: Start new')
+term.write('\x1B[1;3;31mmudpy\x1B[0m $ ')
 
 term.on('key', function (key, e) {
     myBuffer.push(key);
-    term.write(key);
+    if(!del) {
+        term.write(key);
+    }
     if (key === '\r') {
         let keysEntered = myBuffer.join('');
         myBuffer = [];
@@ -28,30 +29,45 @@ term.on('key', function (key, e) {
             term.writeln('\x1b[38;5;10mcommands      |      \x1b[38;5;10mexplanations')
             term.writeln('\x1B[0mhelp          |      shows help menu with commands')
             term.writeln('\x1B[0msubmit        |      submits code for evaluation')
+            term.writeln('\x1B[0mclear         |      clears the console')
             term.write('\x1B[1;3;31mmudpy\x1B[0m $ ')
-        }
-        else if (keysEntered.valueOf() === "submit") {
+        } else if (keysEntered.valueOf() === "submit") {
             document.getElementById("submit").click();
+            term.write('\x1B[1;3;31mmudpy\x1B[0m $ ')
+        } else if (keysEntered.valueOf() === "clear") {
+            term.write('\x1b[2K\r');
+            term.clear();
             term.write('\x1B[1;3;31mmudpy\x1B[0m $ ')
         } else {
             term.writeln('\r')
             term.write('\x1B[1;3;31mmudpy\x1B[0m $ ')
         }
     } else if (key === '1' && Boolean(start)) {
+        term.clear();
         term.write('\r');
         term.writeln('\x1b[38;5;33mWelcome back python adventurer\x1B[0m')
         term.writeln('\x1b[38;5;33mYour story continues here\x1B[0m')
         term.write('\x1B[1;3;31mmudpy\x1B[0m $ ')
         start = false;
     } else if (key === '2' && Boolean(start)) {
+        term.clear();
         term.write('\r');
         term.writeln('\x1b[38;5;33mWelcome new python adventurer\x1B[0m')
         term.writeln('\x1b[38;5;33mYour story begins here\x1B[0m')
         term.write('\x1B[1;3;31mmudpy\x1B[0m $ ')
         start = false;
     }
-    else if (key === 8) {
-        myBuffer = myBuffer.slice(0, myBuffer.length -2);
-        term.write("\b \b");
+});
+
+
+//backspace
+term.on('keydown', function(event) {
+    const key = event.key;
+    if (key === "Backspace" || key === "Delete") {
+            term.write('\b \b');
+            del = true;
+    }
+    else {
+        del = false;
     }
 });
