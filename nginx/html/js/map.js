9b4b1b74a09ddd8b204a446d88a3b4f75e347852
holzit <holzerstefan@hotmail.com>
2021-10-23 21:16:51 +0200

    add non minified css and add a first map proto

---
+++ nginx/html/js/map.js
@@ -0,0 +1,77 @@
+let mapSelector = document.getElementById("map");
+
+let roomsCompleted = [1, 2, 3];
+let playerPosition = 4;
+
+const firstFloor = [1, 2, 3, 4];
+const secondFloor = [6, 7, 8];
+
+let floors = [firstFloor, secondFloor];
+
+const firstFloorAdj = [];
+const secondFloorAdj = [];
+const thirdFloorAdj = ["S"];
+const fourthFloorAdj = [];
+
+const sixthFloorAdj = [];
+const seventhFloorAdj = [];
+const eightFloorAdj = ["N"];
+
+let adjacentFloorOne = [firstFloorAdj, secondFloorAdj, thirdFloorAdj, fourthFloorAdj];
+let adjacentFloorTwo = [sixthFloorAdj, seventhFloorAdj, eightFloorAdj];
+
+let adjacentFloors = [adjacentFloorOne, adjacentFloorTwo];
+
+function drawVertical(fl, i, map) {
+    for (let j = 0; j < 3; j++) {
+        for (let k = 0; k < fl[i].length; k++) {
+            map.push(" ");
+            if (roomIsAdjacent(k, i)) {
+                map.push("|")
+            } else {
+                map.push(" ");
+            }
+            map.push("        ");
+        }
+        map.push("\n");
+    }
+}
+
+function drawMap(roomsC, playerP, fl) {
+    let map = [""];
+    map.push('P = Player, X = completed Rooms');
+    map.push("\n\n");
+    for (let i = 0, size = fl.length; i < size; i++) {
+        let iterations = fl[i].length;
+        for (const element of fl[i]) {
+            if (playerP === element) {
+                map.push("[" + "P" + "]");
+            } else if (roomsC.includes(element)) {
+                map.push("[" + "X" + "]");
+            } else {
+                map.push("[" + " " + "]");
+            }
+            if (--iterations) {
+                map.push("<----->")
+            }
+        }
+        map.push("\n");
+        drawVertical(fl, i, map);
+    }
+    return map.join('');
+}
+
+function roomIsAdjacent(roomNumber, floorNumber) {
+    let floor = adjacentFloors[floorNumber];
+    console.log(floor);
+    let room = floor[roomNumber];
+    console.log(room);
+    if (room.length !== 0) {
+        if (room.includes("S")) {
+            return true;
+        }
+    }
+    return false;
+}
+
+mapSelector.innerHTML = drawMap(roomsCompleted, playerPosition, floors);
