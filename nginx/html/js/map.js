7f18edd0d85607b519e2d820ef28b51cd8144402
holzest3 <holzerstefan@hotmail.com>
2021-11-23 19:34:23 +0100

    refactor map and fix nodelogic

--- nginx/html/js/map.js
+++ nginx/html/js/map.js
@@ -1,66 +1,85 @@
 function drawMap(rc, cr) {
     let mapSelector = document.getElementById("map")
     let map = [""];
-    let floor = [];
     map.push('P = Player, X = completed Rooms')
     map.push("\n\n")
 
-    function draw(room, rooms) {
-        floor.push(rooms[room]);
-        if (rooms[room].adjacent_rooms.east == null) {
-            if(floor[0].adjacent_rooms.west != null) {
-                floor.splice(0, 0, rooms.find(element => element.id === floor[0].adjacent_rooms.west));
-            }
-            let iterations = floor.length;
-            for (const element of floor) {
-                map.push(" " + element.id + "   ")
+    function drawV(floor) {
+        map.push("\n");
+        for (let j = 0; j < 3; j++) {
+            map.push("   ");
+            for (const item of floor) {
+                map.push(" ");
+                if (item.adjacent_rooms.south != null) {
+                    map.push("|")
+                } else {
+                    map.push(" ");
+                }
+                map.push("          ");
             }
             map.push("\n");
-            map.push("   ");
-            for (const element of floor) {
-                console.log(element)
-                console.log(cr)
-                if (cr == element.id) {
-                    map.push("[" + "P" + "]");
-                } else if (rc !== undefined) {
-                    if (rc.includes(element.id)) {
-                        map.push("[" + "X" + "]");
-                    }
-                    else {
-                        map.push("[" + " " + "]");
-                    }
+        }
+    }
+
+    function drawH(floor, iterations) {
+        map.push("\n");
+        map.push("   ");
+        for (const element of floor) {
+            console.log(element)
+            console.log(cr)
+            if (cr === element.id) {
+                map.push("[" + "P" + "]");
+            } else if (rc !== undefined) {
+                if (rc.includes(element.id)) {
+                    map.push("[" + "X" + "]");
                 } else {
                     map.push("[" + " " + "]");
                 }
-                if (--iterations) {
-                    map.push("<------->")
-                }
+            } else {
+                map.push("[" + " " + "]");
             }
-            map.push("\n");
-            for (let j = 0; j < 3; j++) {
-                map.push("   ");
-                for (const item of floor) {
-                    map.push(" ");
-                    console.log("item "+ item);
-                    if (item.adjacent_rooms.south != null) {
-                        map.push("|")
-                    } else {
-                        map.push(" ");
-                    }
-                    map.push("          ");
-                }
-                map.push("\n");
+            if (--iterations) {
+                map.push("<------->")
             }
-            floor = [];
         }
+    }
+
+    function draw(floor) {
+        console.log(floor)
+        let iterations = floor.length;
+        for (const element of floor) {
+            map.push(" " + element.id + "   ")
+        }
+        drawH(floor, iterations);
+        drawV(floor);
     }
 
     axios.get('api/rooms').then(resp => {
         let rooms = resp.data;
-        //console.log(rooms);
+        let floor = [];
+        let drawFloor = true;
         for (let room in rooms) {
-            //console.log(room)
-            draw(room, rooms);
+            drawFloor = true;
+            if (floor.length === 0) {
+                console.log(floor)
+                floor.push(rooms[room])
+                console.log(floor)
+                drawFloor = false
+            }
+            if (rooms[room].adjacent_rooms.east && (!floor.includes(rooms.find(element => element.id === rooms[room].adjacent_rooms.east)))) {
+                floor.push(rooms.find(element => element.id === rooms[room].adjacent_rooms.east))
+                console.log(floor)
+                drawFloor = false
+            }
+            if (rooms[room].adjacent_rooms.west && (!floor.includes(rooms.find(element => element.id === rooms[room].adjacent_rooms.west)))) {
+                floor.splice(0, 0, rooms.find(element => element.id === rooms[room].adjacent_rooms.west));
+                console.log(floor)
+                drawFloor = false
+            }
+            if (Boolean(drawFloor)) {
+                draw(floor);
+                floor = [];
+            }
         }
         mapSelector.innerHTML = map.join('');
     });
